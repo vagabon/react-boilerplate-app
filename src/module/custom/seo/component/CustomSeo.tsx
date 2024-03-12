060c6678db009b22199ac84cd4939b3d5af1fe78
GCL <gonzague.clement@gmail.com>
2024-03-12 14:39:31 +0100

    feat(seo): optim seo

--- src/module/custom/seo/component/CustomSeo.tsx
+++ src/module/custom/seo/component/CustomSeo.tsx
@@ -16,9 +16,9 @@ export interface ICustomSeoProps {
 const CustomSeo: React.FC<ICustomSeoProps> = ({
   title,
   description,
-  image = 'https://blog.vagabond.synology.me/images/logo.png',
+  image = '',
   type = 'webapp',
-  date,
+  date = new Date(),
 }) => {
   const { t } = useAppTranslate();
   const { location } = useAppRouter();
@@ -29,6 +29,9 @@ const CustomSeo: React.FC<ICustomSeoProps> = ({
   }, [location]);
 
   const getImage = useCallback((image: string) => {
+    if (image === '') {
+      return window.location.origin + 'images/logo.png';
+    }
     if (image && !image.includes('http://') && !image.startsWith('https://')) {
       return API_URL + '/download?fileName=' + image;
     }
@@ -39,33 +42,29 @@ const CustomSeo: React.FC<ICustomSeoProps> = ({
     <Helmet data-rh='true' ata-react-helmet='true'>
       <title data-rh='true'>{WEBSITE_TITLE + ' | ' + I18nUtils.translate(t, title as string)}</title>
       <link rel='canonical' href={url} />
-      <meta name='description' content={description} data-rh='true' />
+      <meta name='description' content={I18nUtils.translate(t, description as string)} data-rh='true' />
       <meta property='og:type' content={type} />
-      <meta property='og:title' content={WEBSITE_TITLE + ' | ' + title} />
-      <meta property='og:image' content={getImage(image)} />
+      <meta property='og:title' content={WEBSITE_TITLE + ' | ' + I18nUtils.translate(t, title as string)} />
+      {image && <meta property='og:image' content={getImage(image)} />}
       <meta property='og:description' content={I18nUtils.translate(t, description as string)} />
       <meta name='twitter:creator' content={'@VagabondDev'} />
       <meta name='twitter:card' content={type} />
-      <meta name='twitter:title' content={title} />
-      <meta name='twitter:description' content={description} />
+      <meta name='twitter:title' content={I18nUtils.translate(t, title as string)} />
+      <meta name='twitter:description' content={I18nUtils.translate(t, description as string)} />
       <script type='application/ld+json'>
         {JSON.stringify({
           '@context': 'https://schema.org/',
-          '@type': 'Recipe',
-          name: { title },
-          image: [getImage(image)],
+          '@type': 'WebSite',
+          url: { url },
+          name: { title: I18nUtils.translate(t, title as string) },
+          image: image ? [getImage(image)] : [],
           author: {
             '@type': 'Organization',
             name: 'Vagabond',
           },
-          datePublished: { date },
-          description: { description },
+          datePublished: { date: date.toLocaleString() },
+          description: { description: I18nUtils.translate(t, description as string) },
           keywords: 'vagabond,blog,react,quarkus',
-          aggregateRating: {
-            '@type': 'AggregateRating',
-            ratingValue: '5',
-            ratingCount: '12',
-          },
         })}
       </script>
     </Helmet>
